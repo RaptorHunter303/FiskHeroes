b200d56c7123ad3f281bd1f0aa11904b2bacb4b2
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-10-21 17:46:17 -0400

    Updated ReloadWeapon class
    to 1.15.2

--- src/main/java/com/fiskmods/heroes/common/item/IReloadWeapon.java
+++ src/main/java/com/fiskmods/heroes/common/item/IReloadWeapon.java
@@ -2,10 +2,10 @@ package com.fiskmods.heroes.common.item;
 
 import com.fiskmods.heroes.common.hero.Hero;
 
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 
 public interface IReloadWeapon
 {
-    int getReloadTime(ItemStack stack, EntityPlayer player, Hero hero);
+    int getReloadTime(ItemStack stack, PlayerEntity player, Hero hero);
 }
